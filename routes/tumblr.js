90890661c0d08468d4c05db9f49ddd0c9ff3c6e3
Mickael Goubin <micky0304@gmail.com>
2015-03-11 19:53:32 +0100

    Logging the posts in an array for dates

--- routes/tumblr.js
+++ routes/tumblr.js
@@ -8,11 +8,41 @@ var oauth = {
 
 var blog = new tumblr.Blog('equipe.tumblr.com', oauth);
 
+
 router.get('/', function(req, res, next) {
-  blog.text({limit: 2}, function(error, response) {
+  blog.text({}, function(error, response) {
     if(error) throw new Error(error);
-    console.log(response.posts);
+    
+    var days = [];
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth() +1;
+    var yyyy = today.getFullYear();
+    var todays_post = response.posts;
+    var last_date=undefined;
+    var nbDay=0;
+    var nbPostsForCurrentDay=0;
+    todays_post.forEach(function(post,index,array) {
+      
+      var post_date = post.date.split("-");
+      var date = new Date(post_date[0], (post_date[1] -1), post_date[2].split(" ")[0]);
+      if(date != last_date || last_date == undefined) {
+        last_date = date;
+        nbDay++;
+        nbPostsForCurrentDay=0;
+        var tmp = {date:last_date, posts:[]};
+        days[nbDay]= tmp;
+      }
+      days[nbDay].posts[nbPostsForCurrentDay] = post;
+      nbPostsForCurrentDay++;
+    });
+    
+    console.log(days);
+  
   });
+  
+  
+  
   
   res.send("Hello");
 });
