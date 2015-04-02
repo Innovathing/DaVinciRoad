078b6f3dd765d04737c3fca4894761b46e226324
David Livet <david.livet@orange.fr>
2015-04-02 21:49:52 +0200

    Lag fix

--- js/main.js
+++ js/main.js
@@ -11,7 +11,6 @@ $(document).ready(function() {
         recordHistory: false,
         menu:"#menu",
 
-        css3:false,
         scrollOverflow: true
     });
     $('.material-design-hamburger__icon').on("click",toogleMenu);
