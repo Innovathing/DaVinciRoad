dd81c4eda0ec78f0f84203c728953ed6021078b5
David Livet <david.livet@orange.fr>
2015-04-02 13:37:33 +0200

    Add anchors

--- js/main.js
+++ js/main.js
@@ -2,7 +2,14 @@
 $(document).ready(function() {
     $('main').fullpage({
         sectionSelector: 'article',
+        anchors:['day-0','day-1','day-2'],
+
+        navigation: true,
+        navigationPosition: 'left',
+        navigationTooltips: ['Day 0', 'Day 1', 'Day 2'],
+        recordHistory: false,
+
         css3:false,
-				scrollOverflow: true
+        scrollOverflow: true
     });
 });
