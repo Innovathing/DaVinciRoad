c192d81eca89d7d9ac3b3e3e6a34313247ba52db
David Livet <david.livet@orange.fr>
2015-04-02 20:18:03 +0200

    Add menu responsive

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 
 $(document).ready(function() {
+    var $modal = $('.mask-modal');
     $('main').fullpage({
         sectionSelector: '.section',
         anchors:['day-0','day-1','day-2'],
@@ -8,8 +9,17 @@ $(document).ready(function() {
         navigationPosition: 'left',
         navigationTooltips: ['Day 0', 'Day 1', 'Day 2'],
         recordHistory: false,
+        menu:"#menu",
 
         css3:false,
         scrollOverflow: true
     });
+    $('.material-design-hamburger__icon').on("click",toogleMenu);
+    $modal.on("click",toogleMenu);
+    $('.menu-link').on("click",toogleMenu);
+
+    function toogleMenu(){
+        $modal.toggleClass("background--blur");
+        $("#menu").toggleClass("menu--on");
+    }
 });
