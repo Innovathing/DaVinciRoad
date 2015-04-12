0b97cd4359599c3b32eaddb1ca37e3e02be0236c
Mickael Goubin <micky0304@gmail.com>
2015-04-12 23:46:16 +0200

    Fix #10

--- routes/tumblr.js
+++ routes/tumblr.js
@@ -10,95 +10,136 @@ var city = config.cities;
 var blog = new tumblr.Blog(config.tumblrUrl, config.tumblrOAuthKeys);
 
 router.get('/', function(req, res, next) {
-  blog.posts({}, function(error, response) {
+  var postToPostDate = [];
+  var days = [];
+  var nbPosts = 0;
+  var posts = [];
+  retrievePosts(0,50, posts, res);
+
+});
+
+function retrievePosts(from, limit, posts, res) {
+  console.log("Calling api " + from + " to " + limit);
+    blog.posts({limit:limit, offset:from }, function(error, response) {
     if(error) throw new Error(error);
-    
-    var postToPostDate = [];
-    var days = [];
-    var today = new Date();
-    var dd = today.getDate();
-    var mm = today.getMonth() +1;
-    var yyyy = today.getFullYear();
-    var todays_post = response.posts;
-    var last_date=undefined;
-    var nbDay=-1;
-    var nbPostsForCurrentDay=0;
-    todays_post.forEach(function(post,index,array) {
-      
-      var post_date = post.date.split("-");
-      var date = new Date(post_date[0], (post_date[1] -1), post_date[2].split(" ")[0]);
-      if(last_date==undefined || date.getTime() != last_date.getTime()) {
-        last_date = date;
-        nbDay++;
-        nbPostsForCurrentDay = 0;
-        var tmp = {date:last_date, posts:[]};
-        days[nbDay]= tmp;
-      }
-      var repost = isRepost(post.tags, nbDay+1);
-      if(repost>-1) {
-        if(postToPostDate[repost] == undefined) {
-          postToPostDate[repost] = {posts:[]};
-        }
-        var maxPostForDay = postToPostDate[repost].posts.length;
-        postToPostDate[repost].posts[maxPostForDay] =  post;
-      } else {
-        days[nbDay].posts[nbPostsForCurrentDay] = post;
-        nbPostsForCurrentDay++;
-      }
-    });
-    
-    var data =[];
-    for(var i= nbDay; i>=0; i--) {
-      var nbTimelapse=0, nbPosts=0;
-      var daily_post = days[i].posts;
-      var current_data={posts:[], timelapse:[]};
-      current_data.date = days[i].date;
-      current_data.city = city[nbDay-i];
-      if(current_data.city==undefined) current_data.city="undefined";
-      
-      //If there are any post to post date, add them to daily_post
-      if(postToPostDate[nbDay-i] != undefined) {
-        postToPostDate[nbDay-i].posts.forEach(function(post, index, array) {
-          daily_post[daily_post.length] = post;
-        });
-      }
-      daily_post.forEach(function(post, index,array) {
-          if(post.type== "photo" && post.tags.indexOf("featured") > -1) {
-            current_data.featured = post.photos[0].original_size;
-          }
-          else if(post.type== "photo") {
-            current_data.posts[nbPosts] = {type:"photo", data:{ caption:sanitizeHtml(post.caption, {allowedTags:[]}), photos:post.photos}, id: post.id};
-            nbPosts++;
-          } else if(post.type == "video" && post.tags.indexOf("timelapse") > -1) {
-            var d = {video: post.video_url, preview:post.thumbnail_url};
-            current_data.timelapse[nbTimelapse] = d;
-            nbTimelapse++;
-          } else if(post.type == "video") {
-            var d={caption: sanitizeHtml(post.caption,{allowedTags:[]}), video:post.video_url, preview:post.thumbnail_url};
-            current_data.posts[nbPosts] = {type:"video", data: d};
-            nbPosts++;
-          } else if(post.type =="text") {
-            current_data.posts[nbPosts] = {type:"text", data: {title: post.title, body:sanitizeHtml(post.body)}};
-            nbPosts++;
-          } else if(post.type == "quote") {
-            current_data.posts[nbPosts] = {type:"quote", data:{from:post.source, text:post.text}};
-            nbPosts++;
-          } else if(post.type=="link") {
-            current_data.posts[nbPosts] = {type:"link", data:getDlLink(post.url)};
-            nbPosts++;
-          };
 
+      response.posts.forEach(function(post, index, array) {
+        posts.push(post);
       });
+      if(response.total_posts > (from + limit)) {
+        retrievePosts(from+limit, limit, posts,res);
+      } else {
+        var returned = parseTumblrResult(posts);
+        var days = returned[0];
+        var postToPostDate = returned[1];
+        var data = formatData(days, postToPostDate);
+        res.render('index', {days:data});
+      }
 
-      data[nbDay-i] = current_data;
 
+
+  });
+}
+
+
+function parseTumblrResult(tumblrResult) {
+  // var today = new Date();
+  // var dd = today.getDate();
+  // var mm = today.getMonth() +1;
+  // var yyyy = today.getFullYear();
+  var res =[];
+  var postToPostDate = [];
+
+  var todays_post = tumblrResult;
+  
+  var current = { date: undefined, nbPosts:0};
+
+  var nbDay=res.length;
+  
+  todays_post.forEach(function(post,index,array) {
+    
+    var post_date_splitted = post.date.split("-");
+    var date = new Date(post_date_splitted[0], (post_date_splitted[1] -1), post_date_splitted[2].split(" ")[0]);
+    if(current.date==undefined || date.getTime() != current.date.getTime()) {
+      current.date = date;
+      nbDay++;
+      current.nbPosts = 0;
+      res[nbDay]= {date:current.date, posts:[]};
     }
 
-    console.log(JSON.stringify(data,null));      
-
-    res.render('index', {days:data});
+    var repost = isRepost(post.tags, nbDay+1);
+    if(repost>-1) {
+      if(postToPostDate[repost] == undefined) {
+        postToPostDate[repost] = {posts:[]};
+      }
+      var maxPostForDay = postToPostDate[repost].posts.length;
+      postToPostDate[repost].posts[maxPostForDay] =  post;
+    } else {
+      res[nbDay].posts[current.nbPosts] = post;
+      current.nbPosts++;
+    }
   });
-});
+
+  return [res, postToPostDate];
+
+}
+
+
+function formatData(days, postToPostDate) {
+  var data =[];
+  var nbDay = days.length-1;
+
+  //Let's go through each day parsed... in reverse
+  for(var i= 0; i<nbDay; i++) {
+    var nbTimelapse=0, nbPosts=0;
+    var daily_post = days[nbDay-i].posts;
+    var current_data={posts:[], timelapse:[]};
+    current_data.date = days[nbDay-i].date;
+    current_data.city = city[i];
+    if(current_data.city==undefined) current_data.city="undefined";
+    
+    //If there are any post to post date, add them to daily_post
+    if(postToPostDate[i] != undefined) {
+      postToPostDate[i].posts.forEach(function(post, index, array) {
+        daily_post[daily_post.length] = post;
+      });
+    }
+    daily_post.forEach(function(post, index,array) {
+        if(post.type== "photo" && post.tags.indexOf("featured") > -1) {
+          current_data.featured = post.photos[0].original_size;
+        }
+        else if(post.type== "photo") {
+          current_data.posts[nbPosts] = {type:"photo", data:{ caption:sanitizeHtml(post.caption, {allowedTags:[]}), photos:post.photos}, id: post.id};
+          nbPosts++;
+        } else if(post.type == "video" && post.tags.indexOf("timelapse") > -1) {
+          var d = {video: post.video_url, preview:post.thumbnail_url};
+          current_data.timelapse[nbTimelapse] = d;
+          nbTimelapse++;
+        } else if(post.type == "video") {
+          var d={caption: sanitizeHtml(post.caption,{allowedTags:[]}), video:post.video_url, preview:post.thumbnail_url};
+          current_data.posts[nbPosts] = {type:"video", data: d};
+          nbPosts++;
+        } else if(post.type =="text") {
+          current_data.posts[nbPosts] = {type:"text", data: {title: post.title, body:sanitizeHtml(post.body)}};
+          nbPosts++;
+        } else if(post.type == "quote") {
+          current_data.posts[nbPosts] = {type:"quote", data:{from:post.source, text:post.text}};
+          nbPosts++;
+        } else if(post.type=="link") {
+          current_data.posts[nbPosts] = {type:"link", data:getDlLink(post.url)};
+          nbPosts++;
+        };
+
+    });
+  console.log(JSON.stringify(current_data));
+
+
+      data[i] = current_data;
+
+  }
+  return data;
+}
+
 
 function isRepost(array,tagMax) {
   for(var i = 0; i < array.length;i++) {
