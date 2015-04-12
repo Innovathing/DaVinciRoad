f6e46215ed02eb22ee9b90170c9b3de57175a132
David Livet <david.livet@orange.fr>
2015-04-12 22:25:41 +0200

    Add Big Map

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,8 @@
 
 $(document).ready(function() {
     var $modal = $('.mask-modal');
-    var anchors = [];
-    var tooltips = [];
+    var anchors = ["home"];
+    var tooltips = ["Home"];
 
     if(days == undefined){
         console.error("Unknown days var");
@@ -10,8 +10,8 @@ $(document).ready(function() {
     }
 
     for(var i = 0; i<days.length; i++){
-        anchors[i] = "day-"+days[i].id;
-        tooltips[i] = "Day "+days[i].id+" - "+days[i].city;
+        anchors[i+1] = "day-"+days[i].id;
+        tooltips[i+1] = "Day "+days[i].id+" - "+days[i].city;
     }
 
     $('main').fullpage({
@@ -77,7 +77,9 @@ var maps = {
        if(google != undefined && this.$current_article != null){
            this.$current_article.find("map.unload").each(function(){
                $(this).removeClass("unload");
-               var map = new google.maps.Map(this, null);
+               var map = new google.maps.Map(this, {
+                   scrollwheel: false
+               });
 
                var ctaLayer = new google.maps.KmlLayer({
                    url: $(this).data("src")
@@ -86,6 +88,56 @@ var maps = {
                });
                ctaLayer.setMap(map);
            });
+           var homemap = $("#home-map.unload");
+           if(homemap != undefined){
+               var bounds = new google.maps.LatLngBounds(null);
+               if(homemap.hasClass("unload")){
+
+                   homemap.removeClass("unload");
+                   var day = [];
+                   day[0] = new google.maps.LatLng(43.6046879,1.4445879);
+                   bounds.extend(day[0]);
+                   day[1] = new google.maps.LatLng(44.276439,9.4009036);
+                   bounds.extend(day[1]);
+                   day[2] = new google.maps.LatLng(43.7694729,11.2549453);
+                   bounds.extend(day[2]);
+                   day[3] = new google.maps.LatLng(41.9028509,12.4964703);
+                   bounds.extend(day[3]);
+                   day[4] = new google.maps.LatLng(43.6158298,13.5189147);
+                   bounds.extend(day[4]);
+                   day[5] = new google.maps.LatLng(45.4408535,12.3155329);
+                   bounds.extend(day[5]);
+                   day[6] = new google.maps.LatLng(45.0703088,7.6868731);
+                   bounds.extend(day[6]);
+
+
+                   var map = new google.maps.Map(window.document.getElementById("home-map"), {
+                       scrollwheel: false,
+                       disableDefaultUI: true,
+                       center:day[1]
+                   });
+
+
+                   $("map").each(function(){
+                       var ctaLayer = new google.maps.KmlLayer({
+                           url: $(this).data("src"),
+                           preserveViewport:true,
+                           suppressInfoWindows:true
+                       },null);
+                       ctaLayer.setMap(map);
+                   });
+
+                   for(var i = 0; i<day.length; i++){
+                       new google.maps.Marker({
+                           position: day[i],
+                           map: map
+                       });
+                   }
+                   map.fitBounds(bounds);
+                   map.panToBounds(bounds);
+
+               }
+           }
        }
    }
 
